e04b6a6734bd6c5ee86e60c23c316cee5a4dc991
Anton Stepanov <iamusualguy@users.noreply.github.com>
2018-06-18 23:29:22 +0300

    leave as is

--- js/index.js
+++
@@ -1,5 +0,0 @@
-        $.get("https://api.myjson.com/bins/2jpm0", function (data, textStatus, jqXHR) {
-        var json = JSON.stringify(data);
-        var obj = jQuery.parseJSON(json);
-    $("#label").html(obj.primeLabel);
-      });
