223907d7130c0df0f16192f628f618b3ae39b75c
Anton Stepanov <iamusualguy@users.noreply.github.com>
2018-06-18 23:21:07 +0300

    move ghost to blog folder

---
+++ js/index.js
@@ -0,0 +1,5 @@
+        $.get("https://api.myjson.com/bins/2jpm0", function (data, textStatus, jqXHR) {
+        var json = JSON.stringify(data);
+        var obj = jQuery.parseJSON(json);
+    $("#label").html(obj.primeLabel);
+      });
